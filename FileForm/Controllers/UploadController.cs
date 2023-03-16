17f1893f1f751f08e4cd6b85e97e58f49a84d160
Anton Manikin <anton.manikin@gmail.com>
2023-03-16 19:20:53 +0200

    initial commit

---
+++ FileForm/Controllers/UploadController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileForm.Controllers;
+
+[ApiController]
+[Route("blob")]
+public class UploadController : ControllerBase
+{
+    [HttpGet("/upload")]
+    public IActionResult UploadFile(IFormFile file, string email)
+    {
+        return Ok();
+    }
+}
+
